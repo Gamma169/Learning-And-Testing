9e0f6fa7d19b877b0d4e6955a5ebe6468edf2e7c
Rienzi Gokea <RGokea@gmail.com>
2017-02-15 21:18:44 -0500

    Finished RadixSort

---
+++ Java/Sorting/InsertionSort.java
@@ -0,0 +1,63 @@
+public class InsertionSort {
+	
+	/*
+	public static void Insert(int[] arr, int a, int inPlace) {
+		for (int i=arr.length-1; i>inPlace; i--)
+			arr[i] = arr[i-1];
+		arr[inPlace] = a;
+	}
+
+	public static void InsertInPosition(int[] arr, int a) {
+		int inPlace = arr.length-1;
+		while(a<arr[inPlace] && inPlace>0) {
+			arr[inPlace] = arr[inPlace - 1]
+			inPlace--;
+		}
+		arr[inPlace] = a;
+	}
+	*/
+
+	public static void InsertionSortArray(int[] arr) {
+		for(int i=1; i<arr.length; i++) {
+			int j = i;
+			while(j > 0 && arr[j-1] > arr[j]){
+				Swap(arr, j-1, j);
+				j--;
+			}
+		}
+	}
+
+	public static void InsertionSortArray2(int[] arr) {
+		for (int i=1; i<arr.length; i++) {
+			for (intj  = i-1, num = arr[i]; j>=0 && num < arr[j]; arr[j] = num, j--)
+				arr[j+1] = arr[j];
+		}
+	}
+
+	public static void Swap(int[] arr, int i1, int i2) {
+		int temp = arr[i1];
+		arr[i1] = arr[i2];
+		arr[i2] = temp;
+	}
+
+	public static void PrintArray(int[] arr) {
+		for (int i=0; i<arr.length; i++)
+			System.out.print(arr[i] + " ");
+		System.out.print("\n");
+	}
+
+
+public static void main(String[] args) {
+
+		int[] array = new int[]{4, 3, 8, 1, 87, 34, 0, 0, 12, 53, 7};
+
+		PrintArray(array);
+		//InsertionSortArray(array);
+		InsertionSortArray2(array);
+		PrintArray(array);
+		//System.out.println(numLoops);
+	}
+
+
+}
+
---
+++ Java/Sorting/Radix.java
@@ -0,0 +1,103 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Radix {
+	
+	private static ArrayList<LinkedList> bucketsEven;
+	private static ArrayList<LinkedList> bucketsOdd;
+	//private static LinkedList<Integer>[] buckets;
+	
+
+
+
+	public static void RadixLSD(int[] arr) {
+		int largest = 0;
+		for (int i=0; i<arr.length; i++) 
+			largest = largest > arr[i] ? largest : arr[i];
+		
+		int longestLength = 1;
+		while ((float)largest / 10 > 1) {
+			largest /= 10;
+			longestLength++;
+		}
+
+		bucketsEven = new ArrayList<LinkedList>();
+		bucketsOdd = new ArrayList<LinkedList>();
+
+		for(int i=0; i<10; i++) {
+			bucketsEven.add(i, new LinkedList<Integer>());
+			bucketsOdd.add(i, new LinkedList<Integer>());
+		}
+		
+		for (int i = 0; i < longestLength; i++) {
+			if (i==0) {
+				for(int j = 0; j < arr.length; j++) {		
+					int buck = (int)(arr[j] % Math.pow(10, i+1));
+					bucketsEven.get(buck).add(arr[j]);
+				}
+			}
+			else {
+				int onBuck = 0;
+				while(onBuck < 10) {
+					if (i % 2 == 0) {
+						if (bucketsOdd.get(onBuck).isEmpty())
+							onBuck++;
+						else {
+							int num = ((Integer)bucketsOdd.get(onBuck).remove()).intValue();
+							int newBuck = (int)((num % Math.pow(10, i+1) / Math.pow(10, i)));
+							bucketsEven.get(newBuck).add(num);
+						}
+					}
+					else {
+						if (bucketsEven.get(onBuck).isEmpty())
+							onBuck++;
+						else {
+							int num = ((Integer)bucketsEven.get(onBuck).remove()).intValue();
+							int newBuck = (int)((num % Math.pow(10, i+1) / Math.pow(10, i)));
+							bucketsOdd.get(newBuck).add(num);
+						}
+					}
+				}
+			}
+		}
+
+		ArrayList<LinkedList> finalBucks;
+		if ((longestLength - 1) % 2 == 0)
+			finalBucks = bucketsEven;
+		else 
+			finalBucks = bucketsOdd;
+
+		int onBuck = 0;
+		int index = 0;
+		while (onBuck < 10) {
+			if (finalBucks.get(onBuck).isEmpty())
+				onBuck++;
+			else {
+				arr[index] = ((Integer)finalBucks.get(onBuck).remove()).intValue();
+				index++;
+			}
+		}
+
+	}
+
+
+
+	public static void PrintArray(int[] arr) {
+		for (int i=0; i<arr.length; i++)
+			System.out.print(arr[i] + " ");
+		System.out.print("\n");
+	}
+
+	public static void main(String[] args) {
+
+		int[] array = new int[]{4, 3, 8, 1, 87, 34, 0, 0, 12, 53, 7, 305, 2540};
+
+		PrintArray(array);
+		RadixLSD(array);
+		PrintArray(array);
+		
+		int i = 4;
+		System.out.println(  (int)((1234 % Math.pow(10, i+1) / Math.pow(10, i)))  );
+	}
+}
